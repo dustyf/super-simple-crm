775395db621386847d81037d4f561335ad51eb47
Dustin Filippini <dusty@dustyf.com>
2017-01-05 07:14:14 -0600

    Adds base plugin files
    Adding base readmes and php file to get the plugin started.

---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Simple CRM ===
+Contributors:      dustyf 
+Donate link:       http://dustyf.com 
+Tags:  
+Requires at least: 4.4  
+Tested up to:      4.4  
+Stable tag:        0.0.0  
+License:           GPLv2  
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html  
+
+== Description ==
+
+Creates a simple CRM Solution for collecting leads.
+
+== Installation ==
+
+=== Manual Installation ===
+
+1. Upload the entire `/simple-crm` directory to the `/wp-content/plugins/` directory.
+2. Activate Simple CRM through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+== Changelog ==
+
+=== 0.0.1 ===
+* First release
+
+== Upgrade Notice ==
+
+=== 0.0.1 ===
+First Release
---
+++ simple-crm.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ * Plugin Name: Simple CRM
+ * Plugin URI:  http://dustyf.com
+ * Description: A Simple CRM Solution to collect leads
+ * Version:     0.0.1
+ * Author:      Dustin Filippini
+ * Author URI:  http://dustyf.com
+ * Donate link: http://dustyf.com
+ * License:     GPLv2
+ * Text Domain: simple-crm
+ * Domain Path: /languages
+ *
+ * @link http://dustyf.com
+ *
+ * @package Simple CRM
+ * @version 0.0.1
+ */
+
+/**
+ * Copyright (c) 2016 Dustin Filippini (email : dusty@dustyf.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+/**
+ * Autoloads files with classes when needed
+ *
+ * @since  NEXT
+ * @param  string $class_name Name of the class being requested.
+ * @return void
+ */
+function simple_crm_autoload_classes( $class_name ) {
+	if ( 0 !== strpos( $class_name, 'SDR_' ) ) {
+		return;
+	}
+
+	$filename = strtolower( str_replace(
+		'_', '-',
+		substr( $class_name, strlen( 'SDR_' ) )
+	) );
+
+	Simple_CRM::include_file( 'includes/class-' . $filename );
+}
+spl_autoload_register( 'simple_crm_autoload_classes' );
+
+/**
+ * Main initiation class
+ *
+ * @since  NEXT
+ */
+final class Simple_CRM {
+
+	/**
+	 * Current version
+	 *
+	 * @var  string
+	 * @since  NEXT
+	 */
+	const VERSION = '0.0.0';
+
+	/**
+	 * URL of plugin directory
+	 *
+	 * @var string
+	 * @since  NEXT
+	 */
+	protected $url = '';
+
+	/**
+	 * Path of plugin directory
+	 *
+	 * @var string
+	 * @since  NEXT
+	 */
+	protected $path = '';
+
+	/**
+	 * Plugin basename
+	 *
+	 * @var string
+	 * @since  NEXT
+	 */
+	protected $basename = '';
+
+	/**
+	 * Singleton instance of plugin
+	 *
+	 * @var Simple_CRM
+	 * @since  NEXT
+	 */
+	protected static $single_instance = null;
+
+	/**
+	 * Creates or returns an instance of this class.
+	 *
+	 * @since  NEXT
+	 * @return Simple_CRM A single instance of this class.
+	 */
+	public static function get_instance() {
+		if ( null === self::$single_instance ) {
+			self::$single_instance = new self();
+		}
+
+		return self::$single_instance;
+	}
+
+	/**
+	 * Sets up our plugin
+	 *
+	 * @since  NEXT
+	 */
+	protected function __construct() {
+		$this->basename = plugin_basename( __FILE__ );
+		$this->url      = plugin_dir_url( __FILE__ );
+		$this->path     = plugin_dir_path( __FILE__ );
+	}
+
+	/**
+	 * Attach other plugin classes to the base plugin class.
+	 *
+	 * @since  NEXT
+	 * @return void
+	 */
+	public function plugin_classes() {
+
+	}
+
+	/**
+	 * Add hooks and filters
+	 *
+	 * @since  NEXT
+	 * @return void
+	 */
+	public function hooks() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Activate the plugin
+	 *
+	 * @since  NEXT
+	 * @return void
+	 */
+	public function _activate() {
+		// Make sure any rewrite functionality has been loaded.
+		flush_rewrite_rules();
+	}
+
+	/**
+	 * Deactivate the plugin
+	 * Uninstall routines should be in uninstall.php
+	 *
+	 * @since  NEXT
+	 * @return void
+	 */
+	public function _deactivate() {}
+
+	/**
+	 * Init hooks
+	 *
+	 * @since  NEXT
+	 * @return void
+	 */
+	public function init() {
+		load_plugin_textdomain( 'simple-crm', false, dirname( $this->basename ) . '/languages/' );
+		$this->plugin_classes();
+	}
+
+	/**
+	 * Magic getter for our object.
+	 *
+	 * @since  NEXT
+	 * @param string $field Field to get.
+	 * @throws Exception Throws an exception if the field is invalid.
+	 * @return mixed
+	 */
+	public function __get( $field ) {
+		switch ( $field ) {
+			case 'version':
+				return self::VERSION;
+			case 'basename':
+			case 'url':
+			case 'path':
+				return $this->$field;
+			default:
+				throw new Exception( 'Invalid ' . __CLASS__ . ' property: ' . $field );
+		}
+	}
+
+	/**
+	 * Include a file from the includes directory
+	 *
+	 * @since  NEXT
+	 * @param  string $filename Name of the file to be included.
+	 * @return bool   Result of include call.
+	 */
+	public static function include_file( $filename ) {
+		$file = self::dir( $filename . '.php' );
+		if ( file_exists( $file ) ) {
+			return include_once( $file );
+		}
+		return false;
+	}
+
+	/**
+	 * This plugin's directory
+	 *
+	 * @since  NEXT
+	 * @param  string $path (optional) appended path.
+	 * @return string       Directory and path
+	 */
+	public static function dir( $path = '' ) {
+		static $dir;
+		$dir = $dir ? $dir : trailingslashit( dirname( __FILE__ ) );
+		return $dir . $path;
+	}
+
+	/**
+	 * This plugin's url
+	 *
+	 * @since  NEXT
+	 * @param  string $path (optional) appended path.
+	 * @return string       URL and path
+	 */
+	public static function url( $path = '' ) {
+		static $url;
+		$url = $url ? $url : trailingslashit( plugin_dir_url( __FILE__ ) );
+		return $url . $path;
+	}
+}
+
+/**
+ * Grab the Simple_CRM object and return it.
+ * Wrapper for Simple_CRM::get_instance()
+ *
+ * @since  NEXT
+ * @return Simple_CRM  Singleton instance of plugin class.
+ */
+function simple_crm() {
+	return Simple_CRM::get_instance();
+}
+
+// Kick it off.
+add_action( 'plugins_loaded', array( simple_crm(), 'hooks' ) );
+
+register_activation_hook( __FILE__, array( simple_crm(), '_activate' ) );
+register_deactivation_hook( __FILE__, array( simple_crm(), '_deactivate' ) );
