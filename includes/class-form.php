e727c1d8a02b7738bb7b754297a880f3df60cd12
Dustin Filippini <dusty@dustyf.com>
2017-01-07 06:04:00 -0600

    Passing in shortcode atts to the rendering of form

--- includes/class-form.php
+++ includes/class-form.php
@@ -47,7 +47,34 @@ class SSCRM_Form {
 		add_action( 'rest_api_init', array( $this, 'rest_api_endpoint' ) );
 	}
 
-	public function render() {
+	public function render( $atts ) {
+		$atts = wp_parse_args( $atts, array(
+			'name_required'       => 'true',
+			'phone_required'      => 'true',
+			'email_required'      => 'true',
+			'budget_required'     => 'true',
+			'message_required'    => 'true',
+			'name_label'          => esc_html__( 'Name:', 'super-simple-crm' ),
+			'phone_label'         => esc_html__( 'Phone Number:', 'super-simple-crm' ),
+			'email_label'         => esc_html__( 'Email Address:', 'super-simple-crm' ),
+			'budget_label'        => esc_html__( 'Desired Budget:', 'super-simple-crm' ),
+			'message_label'       => esc_html__( 'Message:', 'super-simple-crm' ),
+			'name_max_length'     => '',
+			'phone_max_length'    => '',
+			'email_max_length'    => '',
+			'budget_max_length'   => '',
+			'message_max_length'  => '',
+			'message_rows'        => '',
+			'message_cols'        => '',
+			'name_placeholder'    => '',
+			'phone_placeholder'   => '',
+			'email_placeholder'   => '',
+			'budget_placeholder'  => '',
+			'message_placeholder' => '',
+			'thank_you'           => esc_html__( 'Thank you for contacting us. We will contact you shortly to discuss.', 'super-simple-crm' ),
+			'sending_message'     => esc_html__( 'Sending...', 'super-simple-crm' ),
+			'submit_text'         => esc_html__( 'Send', 'sumper-simple-crm' ),
+		) )
 		?>
 		<style>
 			.spinner {
@@ -81,27 +108,37 @@ class SSCRM_Form {
 				display: none;
 			}
 		</style>
-		
+		<?php do_action( 'sscrm_before_form_container', $atts ); ?>
 		<div id="sscrm_form_container">
 			<div class="grayed-out">
 				<div class="done-message">
-					<p>Thank you for contacting us. We will contact you shortly to discuss.</p>
+					<?php do_action( 'sscrm_before_done_message', $atts ); ?>
+					<p><?php echo esc_html( $atts['thank_you'] ); ?></p>
+					<?php do_action( 'sscrm_after_done_message', $atts ); ?>
 				</div>
 				<div class="sending-message">
+					<?php do_action( 'sscrm_before_sending_message', $atts ); ?>
 					<div class="spinner"></div>
-					<p>Sending...</p>
+					<p><?php echo esc_html( $atts['sending_message'] ); ?></p>
+					<?php do_action( 'sscrm_after_sending_message', $atts ); ?>
 				</div>
 			</div>
+			<?php do_action( 'sscrm_before_form', $atts ); ?>
 			<form id="sscrm_form">
-				<label for="sscrm_name">Name: *</label> <input id="sscrm_name" name="sscrm_name" type="text" required /><br/>
-				<label for="sscrm_phone">Phone Number: *</label> <input id="sscrm_phone" name="sscrm_phone" type="tel" required /><br/>
-				<label for="sscrm_email">Email Address: *</label> <input id="sscrm_email" name="sscrm_email" type="email" required /><br/>
-				<label for="sscrm_budget">Desired Budget: *</label> <input id="sscrm_budget" name="sscrm_budget" type="text" required /><br/>
-				<label for="sscrm_message">Message: *</label> <textarea id="sscrm_message" name="sscrm_message" required></textarea>
+				<?php do_action( 'sscrm_form_top', $atts ); ?>
+				<label for="sscrm_name"><?php echo esc_html( $atts['name_label'] ); ?></label><?php if ( $atts['name_required'] ) { ?> <span class="required">*</span><?php } ?> <input id="sscrm_name" name="sscrm_name" type="text" required /><br/>
+				<label for="sscrm_phone"><?php echo esc_html( $atts['phone_label'] ); ?></label><?php if ( $atts['phone_required'] ) { ?> <span class="required">*</span><?php } ?> <input id="sscrm_phone" name="sscrm_phone" type="tel" required /><br/>
+				<label for="sscrm_email"><?php echo esc_html( $atts['email_label'] ); ?></label><?php if ( $atts['email_required'] ) { ?> <span class="required">*</span><?php } ?> <input id="sscrm_email" name="sscrm_email" type="email" required /><br/>
+				<label for="sscrm_budget"><?php echo esc_html( $atts['budget_label'] ); ?></label><?php if ( $atts['budget_required'] ) { ?> <span class="required">*</span><?php } ?> <input id="sscrm_budget" name="sscrm_budget" type="text" required /><br/>
+				<label for="sscrm_message"><?php echo esc_html( $atts['message_label'] ); ?></label><?php if ( $atts['message_required'] ) { ?> <span class="required">*</span><?php } ?> <textarea id="sscrm_message" name="sscrm_message" required></textarea>
 				<?php wp_nonce_field( 'wp_rest' ); ?>
-				<input id="sscrm_submit" name="sscrm_submit" type="submit" value="Send" />
+				<?php do_action( 'sscrm_before_form_submit', $atts ); ?>
+				<input id="sscrm_submit" name="sscrm_submit" type="submit" value="<?php echo esc_html( $atts['submit_text'] ); ?>" />
+				<?php do_action( 'sscrm_after_form_submit', $atts ); ?>
 			</form>
+			<?php do_action( 'sscrm_after_form', $atts ); ?>
 		</div>
+		<?php do_action( 'sscrm_after_form_container', $atts ); ?>
 		<script>
 			jQuery( document ).ready( function( $ ) {
 				$( '#sscrm_form' ).submit( function( e ) {
@@ -124,9 +161,36 @@ class SSCRM_Form {
 		<?php
 	}
 
-	public function shortcode() {
+	public function shortcode( $atts ) {
+		$atts = shortcode_atts( array(
+			'name_required'       => 'true',
+			'phone_required'      => 'true',
+			'email_required'      => 'true',
+			'budget_required'     => 'true',
+			'message_required'    => 'true',
+			'name_label'          => esc_html__( 'Name:', 'super-simple-crm' ),
+			'phone_label'         => esc_html__( 'Phone Number:', 'super-simple-crm' ),
+			'email_label'         => esc_html__( 'Email Address:', 'super-simple-crm' ),
+			'budget_label'        => esc_html__( 'Desired Budget:', 'super-simple-crm' ),
+			'message_label'       => esc_html__( 'Message:', 'super-simple-crm' ),
+			'name_max_length'     => '',
+			'phone_max_length'    => '',
+			'email_max_length'    => '',
+			'budget_max_length'   => '',
+			'message_max_length'  => '',
+			'message_rows'        => '',
+			'message_cols'        => '',
+			'name_placeholder'    => '',
+			'phone_placeholder'   => '',
+			'email_placeholder'   => '',
+			'budget_placeholder'  => '',
+			'message_placeholder' => '',
+			'thank_you'           => esc_html__( 'Thank you for contacting us. We will contact you shortly to discuss.', 'super-simple-crm' ),
+			'sending_message'     => esc_html__( 'Sending...', 'super-simple-crm' ),
+			'submit_text'         => esc_html__( 'Send', 'sumper-simple-crm' ),
+		), $atts, 'sscrm_form' );
 		ob_start();
-		$this->render();
+		$this->render( $atts );
 		$form = ob_get_clean();
 
 		return apply_filters( 'sscrm_shortcode_form', $form );
